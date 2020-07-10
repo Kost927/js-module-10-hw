0bdb2dc11108edf33c0900cfb279a9b6ea6ca555
Kostya Polishko <kost927@gmail.com>
2020-07-10 14:41:04 +0300

    init1

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -1,63 +1,3 @@
-import { cartModal } from '../modals/cartModal';
-
-const refs = {
-  cartBtn: document.querySelector('.cart'),
-};
-
-export const cart = {
-  order: [],
-  totalSum: 0,
-  totalQuantity: 0,
-};
-
-export const addToCart = dish => {
-  const result = cart.order.find(item => item.name === dish.name);
-  if (!result) {
-    cart.order = [
-      ...cart.order,
-      {
-        id: dish.id,
-        name: dish.name,
-        price: dish.price,
-        quantity: 1,
-      },
-    ];
-  } else {
-    cart.order = cart.order.map(item => {
-      if (item.name === dish.name) {
-        return {
-          ...item,
-          quantity: item.quantity + 1,
-        };
-      } else {
-        return item;
-      }
-    });
-  }
-  getTotal();
-};
-
-const removeFromCart = id => {
-  cart.order = cart.order.filter(dish => dish.id !== id);
-  getTotal();
-};
-
-const getTotal = () => {
-  cart.totalSum = cart.order.reduce((acc, dish) => {
-    acc += dish.price * dish.quantity;
-    return acc;
-  }, 0);
-  cart.totalQuantity = cart.order.reduce((acc, dish) => {
-    acc += dish.quantity;
-    return acc;
-  }, 0);
-};
-
-refs.cartBtn.addEventListener('click', cartModal);
-//====
-
-///====
-
 // import * as basicLightbox from 'basiclightbox';
 // import dishes from '../menu.json';
 // const cartRef = document.querySelector('.cart');
@@ -95,28 +35,32 @@ refs.cartBtn.addEventListener('click', cartModal);
 // const cart = {
 //   products: [],
 // };
+// let newProduct = {
+//   name: '',
+//   price: 0,
+//   id: '',
+//   quantity: 0,
+// };
 
 // function addToCartFn(e) {
-//   console.log(e.target);
 //   const dishId = e.target.previousElementSibling.dataset.id;
 
 //   const chosenDish = dishes.find(dish => dish.id === dishId);
 
 //   for (let prod of cart.products) {
-//     if (prod.id === dishId) {
-//       prod.quantity += 1;
-//       let newArr = [...cart.products];
-//       cart.products = newArr;
-//       console.log(cart.products);
+//     if (prod.id !== dishId) {
+//       newProduct = {
+//         name: chosenDish.name,
+//         price: chosenDish.price,
+//         id: chosenDish.id,
+//         quantity: 1,
+//       };
+//     } else {
+//       const newCartProducts = cart.products.map(prod => (prod.quantity += 1));
+//       console.log(newCartProducts);
 //     }
 //   }
-//   const newProduct = {
-//     name: chosenDish.name,
-//     price: chosenDish.price,
-//     id: chosenDish.id,
-//     quantity: 1,
-//   };
 //   cart.products = [newProduct, ...cart.products];
 
-//   //   console.log(cart.products);
+//   // console.log(cart.products);
 // }
